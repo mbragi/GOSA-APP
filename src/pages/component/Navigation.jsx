6d03f75a950588e24d07d792af9cda080e4edd98
ambijames <emmaabison@gmail.com>
2022-08-31 00:53:50 +0100

    Chore: general update

--- src/pages/component/Navigation.jsx
+++ src/pages/component/Navigation.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import routes from '../../routes';
+// import routes from '../../routes';
 import logo from '../../assets/logo.png';
 
 
--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -8,7 +8,7 @@ function Gallery() {
         <Navigation />
         <div className="container px-5 py-24 mx-auto ">
           <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-3xl text-6xl font-medium title-font mb-4 text-white">This is our Gallery of pictures</h1>
+            <h1 className="sm:text-3xl text-6xl font-medium title-font mb-4 text-white">Pictures of our recent events and programs</h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-white">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
           </div>
           <div className="flex flex-wrap -m-4">
