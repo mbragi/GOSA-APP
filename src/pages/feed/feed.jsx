d0d25adb79c971b99c56fc3cf4d1a1f615aaad67
unknown <spontaneousmanji@gmail.com>
2022-09-27 23:26:13 +0100

    feat: handling event for text modal

--- src/pages/feed/feed.jsx
+++ src/pages/feed/feed.jsx
@@ -29,19 +29,19 @@ function Feed() {
   fetchPost()
  }, [])
 
- const createText = (e) => {
+ const modalText = (e) => {
   e.preventDefault();
   e.stopPropagation();
   setFeedbackModalOpen(true)
  }
- const createMedia = (e) => {
+ const modalMedia = (e) => {
   e.preventDefault();
   e.stopPropagation();
   setModalOpen(true)
  }
 
- async function httpCreatePost() {
-  const [name, value] = e.target
+ function createPost(e) {
+  const { name, value } = e.target
   const req = { ...data }
   req[name] = value
   console.log(req)
@@ -78,7 +78,7 @@ function Feed() {
 
            {/* video && photo && audio url */}
 
-           <form onSubmit={httpCreatePost} className="m-1.5">
+           <form className="m-1.5">
             {/* Start */}
             <ModalBasic id="feedback-modal" modalOpen={modalOpen} setModalOpen={setModalOpen} title="select media">
              {/* Modal content */}
@@ -106,18 +106,18 @@ function Feed() {
            </form >
 
            {/* textDescription */}
-           <form onSubmit={httpCreatePost} className="m-1.5">
+           <form className="m-1.5">
             <ModalBasic id="feedback-modal" modalOpen={feedbackModalOpen} setModalOpen={setFeedbackModalOpen} title="Create Post">
              {/* Modal content */}
              <div className="px-5 py-4">
               <div className="space-y-3">
                <div>
                 <label className="block text-sm font-medium mb-1" htmlFor="name">Title <span className="text-rose-500">*</span></label>
-                <input name='title' className="form-input w-full px-2 py-1" type="text" required />
+                <input name='title' className="form-input w-full px-2 py-1" type="text" onChange={createPost} required />
                </div>
                <div>
                 <label className="block text-sm font-medium mb-1" htmlFor="feedback">Message <span className="text-rose-500">*</span></label>
-                <textarea name='textDescription' className="form-textarea w-full px-2 py-1" rows="10" required></textarea>
+                <textarea name='textDescription' className="form-textarea w-full px-2 py-1" onChange={createPost} required></textarea>
                </div>
               </div>
              </div>
@@ -145,7 +145,7 @@ function Feed() {
                className="form-input w-full bg-slate-100 border-transparent focus:bg-white focus:border-slate-300 placeholder-slate-500"
                type="text"
                placeholder="What's happening..."
-               onClick={createText}
+               onClick={modalText}
                name='textDescription'
               />
              </div>
@@ -153,7 +153,7 @@ function Feed() {
             <div className="flex justify-between items-center">
              <div className="grow flex space-x-5">
               <button className="inline-flex items-center text-sm font-medium text-slate-600 hover:text-slate-700"
-               onClick={createMedia}
+               onClick={modalMedia}
               >
                <svg className="w-4 h-4 fill-indigo-400 mr-2" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 0h2v16H0V0Zm14 0h2v16h-2V0Zm-3 7H5c-.6 0-1-.4-1-1V1c0-.6.4-1 1-1h6c.6 0 1 .4 1 1v5c0 .6-.4 1-1 1ZM6 5h4V2H6v3Zm5 11H5c-.6 0-1-.4-1-1v-5c0-.6.4-1 1-1h6c.6 0 1 .4 1 1v5c0 .6-.4 1-1 1Zm-5-2h4v-3H6v3Z" />
@@ -161,7 +161,7 @@ function Feed() {
                <span>Audio</span>
               </button>
               <button className="inline-flex items-center text-sm font-medium text-slate-600 hover:text-slate-700"
-               onClick={createMedia}
+               onClick={modalMedia}
               >
                <svg className="w-4 h-4 fill-indigo-400 mr-2" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6.974 14c-.3 0-.7-.2-.9-.5l-2.2-3.7-2.1 2.8c-.3.4-1 .5-1.4.2-.4-.3-.5-1-.2-1.4l3-4c.2-.3.5-.4.9-.4.3 0 .6.2.8.5l2 3.3 3.3-8.1c0-.4.4-.7.8-.7s.8.2.9.6l4 8c.2.5 0 1.1-.4 1.3-.5.2-1.1 0-1.3-.4l-3-6-3.2 7.9c-.2.4-.6.6-1 .6Z" />
@@ -169,7 +169,7 @@ function Feed() {
                <span>Video</span>
               </button>
               <button className="inline-flex items-center text-sm font-medium text-slate-600 hover:text-slate-700"
-               onClick={createMedia}
+               onClick={modalMedia}
               >
                <svg className="w-4 h-4 fill-indigo-400 mr-2" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9.793 10.002a.5.5 0 0 1 .353.853l-1.792 1.793a.5.5 0 0 1-.708 0l-1.792-1.793a.5.5 0 0 1 .353-.853h3.586Zm5.014-4.63c1.178 2.497 1.833 5.647.258 7.928-1.238 1.793-3.615 2.702-7.065 2.702S2.173 15.092.935 13.3c-1.575-2.28-.92-5.431.258-7.927A2.962 2.962 0 0 1 0 3.002a3 3 0 0 1 3-3c.787 0 1.496.309 2.029.806a5.866 5.866 0 0 1 5.942 0A2.96 2.96 0 0 1 13 .002a3 3 0 0 1 3 3c0 .974-.472 1.827-1.193 2.37Zm-1.387 6.79c1.05-1.522.417-3.835-.055-5.078C12.915 5.89 11.192 2.002 8 2.002s-4.914 3.89-5.365 5.082c-.472 1.243-1.106 3.556-.055 5.079.843 1.22 2.666 1.839 5.42 1.839s4.577-.62 5.42-1.84ZM6.67 6.62c.113.443.102.68-.433 1.442-.535.761-1.06 1.297-1.658 1.297-.597 0-1.08-.772-1.07-1.483.01-.71.916-2.306 1.997-2.306.784 0 1.05.607 1.163 1.05Zm3.824-1.05c1.08 0 1.987 1.596 1.997 2.306.01.71-.473 1.483-1.07 1.483-.598 0-1.123-.536-1.658-1.297-.535-.762-.546-1-.432-1.442.113-.443.38-1.05 1.163-1.05Z" />
