56423fdb6d2b6a29c736ef133e6e6353a25e6103
unknown <spontaneousmanji@gmail.com>
2022-10-06 10:46:06 +0100

    feat : updating application

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,34 @@
-import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storageSession from 'redux-persist/lib/storage/session';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import rootReducers from './root-reducer';
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storageSession from "redux-persist/lib/storage/session";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducers from "./root-reducer";
 
-const middlewares = [thunk]
+const middlewares = [thunk];
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage: storageSession,
-  whitelist: ['auth'] // only keys defined in rootReducer will be persisted
+  whitelist: ["auth"], // only keys defined in rootReducer will be persisted
+};
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger)
-}
+const middleware = applyMiddleware(...middlewares);
 
-const middleware = applyMiddleware(...middlewares)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const persistedReducer = persistReducer(persistConfig, rootReducers);
 
-const persistedReducer = persistReducer(persistConfig, rootReducers)
-
-export const store = createStore(persistedReducer, composeEnhancers(middleware))
+export const store = createStore(
+  persistedReducer,
+  composeEnhancers(middleware)
+);
 export const persistor = persistStore(store);
