a1b7e676df0e8076f96e443c8d15e00f414986bf
unknown <spontaneousmanji@gmail.com>
2022-10-12 02:14:23 +0100

    saving changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,8 +19,7 @@ function Home(props) {
         <>
             <div className='gosa-background'
             // style={{ background: 'linear-gradient(90deg, #a5e29c, #1b7b2c)'}}
-            >
-                <div className="custom-shape-divider-bottom-1661903965">
+            > <div className="custom-shape-divider-bottom-1661903965">
                     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                         <path d="M1200 0L0 0 598.97 114.72 1200 0z" className="shape-fill"></path>
                     </svg>
