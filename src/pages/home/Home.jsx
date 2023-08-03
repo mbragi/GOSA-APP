33e51e5fe912b2277b001fc22d7b851ebe095da7
Gar Manji Michael <spontaneousmanji@gmail.com>
2023-08-03 11:11:50 +0000

    updating app

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -75,7 +75,7 @@ function Home(props) {
 
                 </main>
                 <footer className='container mx-auto p-6 flex flex-col md:flex-row  items-center justify-between ' >
-                    <p>Built with ❤️️ by Bluehouse.</p>
+                    <p>Built with ❤️️ by Codelary.</p>
                     <div className="flex -mx-6">
                         <a href="#" className="mx-3 hover:opacity-80 duration-150">About us</a> | <a href="#" className="mx-3 hover:opacity-80 duration-150">Privacy</a> | <a href="#" className="mx-3 hover:opacity-80 duration-150">Contact</a>
                     </div>
